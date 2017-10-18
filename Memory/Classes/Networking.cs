bda035f4deadbb835a4f76f1557bcdad1dc56bc5
afterburn <k.karsopawiro@gmail.com>
2017-10-18 10:26:43 +0200

    MemoryServer aangemaakt

---
+++ Memory/Classes/Networking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory.Classes {
+	class Networking {
+		private static UdpClient socket;
+
+		public static void Connect() {
+			socket = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+			socket.BeginReceive(OnPacketReceived, null);
+		}
+
+		private static void OnPacketReceived(IAsyncResult ar) {
+
+			socket.BeginReceive(OnPacketReceived, null);
+		}
+	}
+}
---
+++ MemoryServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryServer {
+	class Program {
+		private static UdpClient socket;
+
+		static void Main(string[] args) {
+			socket = new UdpClient(3000);
+			socket.BeginReceive(OnPacketReceived, null);
+		}
+
+		private static void OnPacketReceived(IAsyncResult ar) {
+			IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+			byte[] receivedBytes = socket.EndReceive(ar, ref clientEP);
+			string rawData = Encoding.ASCII.GetString(receivedBytes);
+
+			Console.WriteLine(rawData);
+
+			socket.BeginReceive(OnPacketReceived, null);
+		}
+	}
+}
