a1187f556ad417adb10a56888f5510a83e75610c
afterburn <k.karsopawiro@gmail.com>
2017-10-19 11:17:22 +0200

    Basisstructuur afgemaakt

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -9,15 +9,9 @@ namespace Memory {
 	static class Program {
 		[STAThread]
 		static void Main() {
-			List<Save> saves = IOHandler.GetSaves();
-			foreach(Save save in saves) {
-				Console.WriteLine(save.Username);
-			}
-
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
-
 		}
 	}
 }
