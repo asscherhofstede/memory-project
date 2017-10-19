24fc7ffa87d73e43068457a2da11ff640d4a9ee9
Gepstra <gerlofrienstra@gmail.com>
2017-10-19 16:01:36 +0200

    Hoofdscherm aangepast

--- Memory/Views/Hoofdscherm.Designer.cs
+++ Memory/Views/Hoofdscherm.Designer.cs
@@ -23,33 +23,125 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(0, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(92, 17);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Hoofdscherm";
-			// 
-			// Hoofdscherm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(282, 253);
-			this.Controls.Add(this.label1);
-			this.Name = "Hoofdscherm";
-			this.Text = "Hoofdscherm";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.MainMenu = new System.Windows.Forms.Label();
+            this.Singleplayer = new System.Windows.Forms.Button();
+            this.LoadGame = new System.Windows.Forms.Button();
+            this.LocalMultiplayer = new System.Windows.Forms.Button();
+            this.OnlineMultiplayer = new System.Windows.Forms.Button();
+            this.ChooseTheme = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.Help = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.AutoSize = true;
+            this.MainMenu.Location = new System.Drawing.Point(230, 86);
+            this.MainMenu.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(60, 13);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "Main Menu";
+            this.MainMenu.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Singleplayer
+            // 
+            this.Singleplayer.Location = new System.Drawing.Point(201, 155);
+            this.Singleplayer.Name = "Singleplayer";
+            this.Singleplayer.Size = new System.Drawing.Size(120, 29);
+            this.Singleplayer.TabIndex = 1;
+            this.Singleplayer.Text = "Singleplayer";
+            this.Singleplayer.UseVisualStyleBackColor = true;
+            this.Singleplayer.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // LoadGame
+            // 
+            this.LoadGame.Location = new System.Drawing.Point(201, 190);
+            this.LoadGame.Name = "LoadGame";
+            this.LoadGame.Size = new System.Drawing.Size(120, 29);
+            this.LoadGame.TabIndex = 2;
+            this.LoadGame.Text = "Load game";
+            this.LoadGame.UseVisualStyleBackColor = true;
+            this.LoadGame.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // LocalMultiplayer
+            // 
+            this.LocalMultiplayer.Location = new System.Drawing.Point(201, 225);
+            this.LocalMultiplayer.Name = "LocalMultiplayer";
+            this.LocalMultiplayer.Size = new System.Drawing.Size(120, 29);
+            this.LocalMultiplayer.TabIndex = 3;
+            this.LocalMultiplayer.Text = "Local Multiplayer";
+            this.LocalMultiplayer.UseVisualStyleBackColor = true;
+            // 
+            // OnlineMultiplayer
+            // 
+            this.OnlineMultiplayer.Location = new System.Drawing.Point(201, 260);
+            this.OnlineMultiplayer.Name = "OnlineMultiplayer";
+            this.OnlineMultiplayer.Size = new System.Drawing.Size(120, 29);
+            this.OnlineMultiplayer.TabIndex = 4;
+            this.OnlineMultiplayer.Text = "Online Multiplayer";
+            this.OnlineMultiplayer.UseVisualStyleBackColor = true;
+            this.OnlineMultiplayer.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ChooseTheme
+            // 
+            this.ChooseTheme.Location = new System.Drawing.Point(201, 295);
+            this.ChooseTheme.Name = "ChooseTheme";
+            this.ChooseTheme.Size = new System.Drawing.Size(120, 29);
+            this.ChooseTheme.TabIndex = 5;
+            this.ChooseTheme.Text = "Choose Theme";
+            this.ChooseTheme.UseVisualStyleBackColor = true;
+            this.ChooseTheme.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(201, 330);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(120, 29);
+            this.Quit.TabIndex = 6;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = true;
+            // 
+            // Help
+            // 
+            this.Help.Location = new System.Drawing.Point(472, 435);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(69, 29);
+            this.Help.TabIndex = 7;
+            this.Help.Text = "Get Help";
+            this.Help.UseVisualStyleBackColor = true;
+            this.Help.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Hoofdscherm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 476);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.ChooseTheme);
+            this.Controls.Add(this.OnlineMultiplayer);
+            this.Controls.Add(this.LocalMultiplayer);
+            this.Controls.Add(this.LoadGame);
+            this.Controls.Add(this.Singleplayer);
+            this.Controls.Add(this.MainMenu);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Hoofdscherm";
+            this.Text = "Hoofdscherm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Label label1;
-	}
+		private System.Windows.Forms.Label MainMenu;
+        private System.Windows.Forms.Button Singleplayer;
+        private System.Windows.Forms.Button LoadGame;
+        private System.Windows.Forms.Button LocalMultiplayer;
+        private System.Windows.Forms.Button OnlineMultiplayer;
+        private System.Windows.Forms.Button ChooseTheme;
+        private System.Windows.Forms.Button Quit;
+        private System.Windows.Forms.Button Help;
+    }
 }
--- Memory/Views/Hoofdscherm.cs
+++ Memory/Views/Hoofdscherm.cs
@@ -17,5 +17,35 @@ namespace Memory.Views {
 		private void button1_Click(object sender, EventArgs e) {
 			MainForm.LoadView("spelscherm");
 		}
-	}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
